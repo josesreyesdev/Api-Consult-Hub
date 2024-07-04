581646bf864861591b1d11f505fcb77c1e0ba531
Jose SR <josesanchez09190@gmail.com>
2024-07-04 05:47:59 -0600

    added pageableDefault if I want to change params to physician

--- src/main/java/jsrdev/consult_hub/api/controller/PhysicianController.java
+++ src/main/java/jsrdev/consult_hub/api/controller/PhysicianController.java
@@ -8,6 +8,7 @@ import jsrdev.consult_hub.api.physician.RegisterPhysicianData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,8 +23,12 @@ public class PhysicianController {
         physicianRepository.save(new Physician(registerPhysicianData));
     }
 
+    /* Con @PageableDefault() configuramos algunos valores por defecto si es que el front no
+    * envia estos parametros
+    * */
+
     @GetMapping
-    public Page<PhysicianListData> getListOfPhysicians(Pageable pagination) {
+    public Page<PhysicianListData> getListOfPhysicians(@PageableDefault(size = 15) Pageable pagination) {
         return physicianRepository.findAll(pagination)
                 .map(PhysicianListData::new);
     }
