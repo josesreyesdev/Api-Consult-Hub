78eae6ce9ee6212d1028a1dd1494332b941da5cd
Jose SR <josesanchez09190@gmail.com>
2024-07-05 02:09:31 -0600

    added deactivate physician is recommended

--- src/main/java/jsrdev/consult_hub/api/physician/PhysicianRepository.java
+++ src/main/java/jsrdev/consult_hub/api/physician/PhysicianRepository.java
@@ -2,7 +2,6 @@ package jsrdev.consult_hub.api.physician;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhysicianRepository extends JpaRepository<Physician, Long> {
