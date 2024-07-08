e81886830ad715b4a6b6512ac80ce7e0de8f8eae
Jose SR <josesanchez09190@gmail.com>
2024-07-08 03:14:46 -0600

    added authentication controller

---
+++ src/main/java/jsrdev/consult_hub/api/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package jsrdev.consult_hub.api.controller;
+
+import jakarta.validation.Valid;
+import jsrdev.consult_hub.api.domain.user.UserAuthenticationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    // sirve para disparar el proceso de autenticación en spring
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity userAuthentication(
+            @RequestBody @Valid UserAuthenticationData userAuthenticationData
+    ) {
+        Authentication token = new UsernamePasswordAuthenticationToken(
+                userAuthenticationData.login(), userAuthenticationData.pass()
+        );
+
+        authenticationManager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/jsrdev/consult_hub/api/infra/security/SecurityConfiguration.java
+++ src/main/java/jsrdev/consult_hub/api/infra/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package jsrdev.consult_hub.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,5 +22,13 @@ public class SecurityConfiguration {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    // Funcion para inyectar al AuthenticationManager en mi authenticationController
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
