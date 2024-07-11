d0dc2236280f90f2a58d9b99c66f8e05a507d866
Jose SR <josesanchez09190@gmail.com>
2024-07-10 21:47:24 -0600

    added token validation

--- src/main/java/jsrdev/consult_hub/api/infra/security/SecurityFilter.java
+++ src/main/java/jsrdev/consult_hub/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +12,9 @@ import java.io.IOException;
 
 @Component // Component: Es el esteretipo mas generico de spring para definir un componente
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
     
     @Override
     protected void doFilterInternal(
@@ -29,6 +33,8 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
 
         token = token.replace("Bearer ", "");
+
+        System.out.println(tokenService.getSubject(token));
 
         //paso el request y el response a la cadena de filtros
         filterChain.doFilter(request, response);
