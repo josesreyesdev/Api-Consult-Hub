25fcc959c79999814e83dd88f177d6216e3f3440
Jose SR <josesanchez09190@gmail.com>
2024-07-12 21:14:27 -0600

    added applying SOLID principles

--- src/main/java/jsrdev/consult_hub/api/domain/patient/PatientRepository.java
+++ src/main/java/jsrdev/consult_hub/api/domain/patient/PatientRepository.java
@@ -5,14 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Page<Patient> findByActiveTrue(Pageable pagination);
 
     @Query("""
-        SELECT p.active FROM Patient p
-        WHERE p.id = :idPatient
-    """)
+                SELECT p.active FROM Patient p
+                WHERE p.id = :idPatient
+            """)
     Boolean findActiveByID(Long idPatient);
 }
