a7eb73ed373f9b315b3f990486aeeb5dd9e45109
Jose SR <josesanchez09190@gmail.com>
2024-07-14 02:01:03 -0600

    added spring doc

--- src/main/java/jsrdev/consult_hub/api/infra/security/SecurityConfiguration.java
+++ src/main/java/jsrdev/consult_hub/api/infra/security/SecurityConfiguration.java
@@ -28,12 +28,14 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests( auth -> auth.requestMatchers(HttpMethod.POST, "/login")
-                        .permitAll().anyRequest().authenticated()
+                .authorizeHttpRequests( auth -> auth
+                    .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                    .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                            .anyRequest()
+                            .authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-
     }
 
     // Funcion para inyectar al AuthenticationManager en mi authenticationController
